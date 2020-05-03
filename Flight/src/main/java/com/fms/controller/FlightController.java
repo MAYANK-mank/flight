1f85677a708fb2fae5ce6b57148a21e682fde026
MAYANK-mank <60684701+MAYANK-mank@users.noreply.github.com>
2020-05-03 15:47:54 +0530

    Update FlightController.java

--- Flight/src/main/java/com/fms/controller/FlightController.java
+++ Flight/src/main/java/com/fms/controller/FlightController.java
@@ -2,7 +2,7 @@ package com.fms.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fms.dto.Flight;
@@ -10,13 +10,13 @@ import com.fms.service.FlightService;
 
 @RestController
 public class FlightController {
-
+	
 	FlightService flightService;
 	
-	@GetMapping("/addflight")
-	private List<Flight> getFlight()
+	@RequestMapping("/addflight")
+	private List<Flight> getAllFlights()
 	{
-		return flightService.addFlight();
+		return flightService.getAllFlights();
 	}
 
 }
