34c797a541aa3035488a1ed83aa5b29bffc4872e
MAYANK-mank <60684701+MAYANK-mank@users.noreply.github.com>
2020-05-03 15:49:56 +0530

    Update FlightServiceImpl.java

--- Flight/src/main/java/com/fms/service/FlightServiceImpl.java
+++ Flight/src/main/java/com/fms/service/FlightServiceImpl.java
@@ -3,12 +3,12 @@ package com.fms.service;
 import com.fms.dao.FlightDAO;
 import com.fms.dto.Flight;
 
-public class FlightServiceImpl implements FlightService{
+public abstract class FlightServiceImpl implements FlightService{
 	FlightDAO flightDAO;
 	
 	@Override
 	public Flight addFlight(Flight flight) {
-		return null;
+		return flight;
 	}
 
 }
