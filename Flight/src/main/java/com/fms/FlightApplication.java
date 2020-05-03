6c35a95f017fb20acbb047406f1112530d3ec1ba
MAYANK-mank <60684701+MAYANK-mank@users.noreply.github.com>
2020-05-03 15:50:56 +0530

    Update FlightApplication.java

--- Flight/src/main/java/com/fms/FlightApplication.java
+++ Flight/src/main/java/com/fms/FlightApplication.java
@@ -2,8 +2,9 @@ package com.fms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class FlightApplication {
 
 	public static void main(String[] args) {
