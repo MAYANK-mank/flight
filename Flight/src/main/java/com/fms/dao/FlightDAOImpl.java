adb1c4946f4e1420f28d75955c0a94245dd5ca2a
MAYANK-mank <60684701+MAYANK-mank@users.noreply.github.com>
2020-05-04 18:09:16 +0530

    Update FlightDAOImpl.java

--- Flight/src/main/java/com/fms/dao/FlightDAOImpl.java
+++ Flight/src/main/java/com/fms/dao/FlightDAOImpl.java
@@ -1,21 +1,20 @@
 package com.fms.dao;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+
 import com.fms.dto.Flight;
 
 public class FlightDAOImpl implements FlightDAO{
 	
-	private List<Flight> flightList= new ArrayList<Flight>();
+	public List<Flight> flightList= new ArrayList<Flight>();
+	
 	@Override
 	public Flight addFlight(Flight flight)
 	{
-		flight.setFlightNumber(new BigInteger("2490"));
-		flight.setFlightModel("Airbus A380");
-		flight.setCarrierName("Royal Navy");
-		flight.setSeatCapacity(150);
+		
 		flightList.add(flight);
+		System.out.println(flightList);
 		return flight;
 	}
 	
