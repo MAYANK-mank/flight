fe9ff3bd799a835595df327de9bbe58518374253
MAYANK-mank <60684701+MAYANK-mank@users.noreply.github.com>
2020-05-04 18:07:37 +0530

    Update FlightApplication.java

--- Flight/src/main/java/com/fms/FlightApplication.java
+++ Flight/src/main/java/com/fms/FlightApplication.java
@@ -2,13 +2,29 @@ package com.fms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+import com.fms.dao.FlightDAO;
+import com.fms.dao.FlightDAOImpl;
+import com.fms.service.FlightService;
+import com.fms.service.FlightServiceImpl;
+
+@SpringBootApplication
 public class FlightApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightApplication.class, args);
+	}
+	@Bean
+	public FlightService getFlightService()
+	{
+		return new FlightServiceImpl();
+	}
+	
+	@Bean
+	public FlightDAO getFlightDAO()
+	{
+		return new FlightDAOImpl();
 	}
 
 }
